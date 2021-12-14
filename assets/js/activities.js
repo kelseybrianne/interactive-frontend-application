c042a871600459b059a5ec641dd3b4a03594b891
Kelsey Mailau <kelseymailau@gmail.com>
2021-12-13 18:16:17 -0800

    fix index layout to have white background

--- assets/js/activities.js
+++ assets/js/activities.js
@@ -1,11 +1,11 @@
 var requestURL
 
-  // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function () {
-    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    $(".navbar-burger").toggleClass("is-active");
-    $(".navbar-menu").toggleClass("is-active");
-  });
+// Check for click events on the navbar burger icon
+$(".navbar-burger").click(function () {
+// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+$(".navbar-burger").toggleClass("is-active");
+$(".navbar-menu").toggleClass("is-active");
+});
 
 var fetchActivity = function() {
     fetch(requestURL)
