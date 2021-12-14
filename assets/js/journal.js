83e203330f4bffc37933c243f23d6418bd9c06c9
Christina Bohn <christinajbohn@gmail.com>
2021-12-13 21:41:22 -0800

    Added function to save date and entry to local storage.

--- assets/js/journal.js
+++ assets/js/journal.js
@@ -4,3 +4,26 @@ $( function() {
 } );
 
 
+// function saveJournalEntry () {
+//     var entry = document.getElementById('journal-entry').value();
+//     if(entry !== '') {
+//         var journalEntry = JSON.parse(window.localStorage.getItem('journalEntry')) || [];
+//         var savedEntry = {
+//             date: date
+//             entry: entry
+//         }
+//         journalEntry.push(savedEntry)
+//         window.localStorage.setItem('journalEntry', JSON.stringify(journalEntry));
+//     }
+// }
+
+$("#save-btn").on("click", function (event) {
+    event.preventDefault();
+    var entry = $(this).siblings("#journal-entry").val();
+    var date = $("#datepicker").val();
+    localStorage.setItem("entry", entry);
+    localStorage.setItem("date", date);
+
+    console.log(date);
+    console.log(entry);
+  });
