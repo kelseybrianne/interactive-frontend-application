d3eb4b7d848f3f61cfe0ba8a8db9566ff8e98f57
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-17 20:29:37 -0800

    was on wrong branch

--- assets/js/journal.js
+++ assets/js/journal.js
@@ -17,13 +17,19 @@ $("#save-btn").on("click", function (event) {
   event.preventDefault();
   let entry = $(this).siblings("#journal-entry").val();
   let date = $("#datepicker").val();
+  if(date == null || date==""){
+    return false
+  };
+  if (entry == null || entry ==""){
+    return false
+  };
   
   let storedEntryObj = {
-    'entry': '',
-    'date': ''
+    'date': '',
+    'entry': ''
   }
-  storedEntryObj.entry = entry;
   storedEntryObj.date = date;
+  storedEntryObj.entry = entry;
   storedText.push(storedEntryObj)
   console.log('storedText', storedText)
   localStorage.setItem("storedEntry", JSON.stringify(storedText));
@@ -49,3 +55,23 @@ function displayEntry(){
     $('#entry-list').append(row)
   }
 }
+function convertDate(d) {
+  var p = d.split("/");
+  return +(p[2]+p[1]+p[0]);
+}
+
+function sortByDate() {
+  var tbody = document.querySelector("#entry-list");
+  // get trs as array for ease of use
+  var rows = [].slice.call(tbody.querySelectorAll("tr"));
+  
+  rows.sort(function(a,b) {
+    return convertDate(a.cells[0].innerHTML) - convertDate(b.cells[0].innerHTML);
+  });
+  
+  rows.forEach(function(v) {
+    tbody.appendChild(v); // note that .appendChild() *moves* elements
+  });
+}
+
+document.getElementById("journ-btn").addEventListener("click", sortByDate);
